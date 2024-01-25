7802a0e41a765cf5a945493c944214b14ce936b8
Frida Jacobsson <fridaja@axis.com>
2024-01-25 14:12:02 +0100

    Add dev functionality

---
+++ plugins/analytics-module-umami/dev/Playground.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from '@backstage/core-components';
+
+export const Playground = () => {
+  return (
+    <>
+      <Link to="#clicked">Click Here</Link>
+    </>
+  );
+};
