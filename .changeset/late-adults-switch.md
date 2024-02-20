b3faeda63fe6cd85a647dd35acacd6fc79ec36d4
Frida Jacobsson <frida.jacobsson@axis.com>
2024-02-20 08:33:58 +0100

    Dialog (#81)
    * Give Readme Dialog min width
    
    * Add changeset
    
    ---------
    
    Co-authored-by: Frida Jacobsson <fridaja@axis.com>

---
+++ .changeset/late-adults-switch.md
@@ -0,0 +1,5 @@
+---
+'@axis-backstage/plugin-readme': patch
+---
+
+Fixed so that Readme Dialog always have a min width. In that case, if it is not smuch markdown content, the dialog wont be weirdly small.
--- plugins/readme/src/components/ReadmeDialog/ReadmeDialog.tsx
+++ plugins/readme/src/components/ReadmeDialog/ReadmeDialog.tsx
@@ -4,6 +4,7 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
 import { FetchComponent } from '../FetchComponent';
 
 type Props = {
@@ -21,7 +22,9 @@ export const ReadmeDialog = ({ open, onClose }: Props) => {
     >
       <DialogTitle>README</DialogTitle>
       <DialogContent dividers>
-        <FetchComponent />
+        <Box minWidth={650}>
+          <FetchComponent />
+        </Box>
       </DialogContent>
       <DialogActions>
         <Button
