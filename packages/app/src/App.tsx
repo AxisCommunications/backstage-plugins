f9f1dd5a46cac574f780bcbc57403414554f21f1
Frida Jacobsson <fridaja@axis.com>
2024-06-20 11:31:29 +0200

    Fix auth provider

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -26,16 +26,37 @@ import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInPage,
+} from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 import { StatuspagePage } from '@axis-backstage/plugin-statuspage';
-import { VacationCalendarPage } from '@internal/backstage-plugin-vacation-calendar';
+import { VacationCalendarPage } from '@axis-backstage/plugin-vacation-calendar';
+import { microsoftAuthApiRef } from '@backstage/core-plugin-api';
 
 const app = createApp({
+  components: {
+    SignInPage: props => (
+      <SignInPage
+        {...props}
+        providers={[
+          'guest',
+          {
+            id: 'microsoft-auth-provider',
+            title: 'Microsoft',
+            message: 'Sign in using Microsoft',
+            apiRef: microsoftAuthApiRef,
+          },
+        ]}
+      />
+    ),
+  },
   apis,
   bindRoutes({ bind }) {
     bind(catalogPlugin.externalRoutes, {
