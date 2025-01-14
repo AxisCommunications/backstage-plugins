1f7f4fab1d77c3f301f6422c23a4e42796c903c5
Niklas Aronsson <niklasar@axis.com>
2025-01-14 09:56:41 +0100

    fix: add 'config.d.ts' to files

---
+++ .changeset/nine-tips-carry.md
@@ -0,0 +1,5 @@
+---
+'@axis-backstage/plugin-jira-dashboard': patch
+---
+
+Add the "config.d.ts" file to the "files" in package.json.
--- plugins/jira-dashboard/config.d.ts
+++ plugins/jira-dashboard/config.d.ts
@@ -2,19 +2,11 @@ export interface Config {
   /**
    * Configuration options for the Jira Dashboard plugin
    */
-  jiraDashboard:
-    | {
-        /**
-         * Optional annotation prefix for retrieving a custom annotation. Defaut value is jira.com
-         * @visibility frontend
-         */
-        annotationPrefix?: string;
-      }
-    | {
-        /**
-         * Optional annotation prefix for retrieving a custom annotation. Defaut value is jira.com
-         * @visibility frontend
-         */
-        annotationPrefix?: string;
-      };
+  jiraDashboard: {
+    /**
+     * Optional annotation prefix for retrieving a custom annotation. Defaut value is jira.com
+     * @visibility frontend
+     */
+    annotationPrefix?: string;
+  };
 }
