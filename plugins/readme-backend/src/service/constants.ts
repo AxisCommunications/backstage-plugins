a80d8215e16825232e5e5b5eb101ba382568a323
sivarao229 <33264369+sivarao229@users.noreply.github.com>
2025-03-04 18:35:42 -0500

    Reorder README types in constants for consistency

--- plugins/readme-backend/src/service/constants.ts
+++ plugins/readme-backend/src/service/constants.ts
@@ -4,9 +4,9 @@ import { FileType } from './types';
 export const NOT_FOUND_PLACEHOLDER = 'NOT_FOUND';
 
 export const README_TYPES: FileType[] = [
-  { name: 'README', type: 'text/plain' },
   { name: 'README.md', type: 'text/markdown' },
+  { name: 'README.MD', type: 'text/markdown' },
+  { name: 'README', type: 'text/plain' },
   { name: 'README.rst', type: 'text/plain' },
   { name: 'README.txt', type: 'text/plain' },
-  { name: 'README.MD', type: 'text/markdown' },
 ];
