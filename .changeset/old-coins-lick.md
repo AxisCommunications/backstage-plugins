39d98e445d0e5c483446715b818a6e758f932e63
anicke <9333839+anicke@users.noreply.github.com>
2025-03-27 09:00:11 +0100

    feat(jira-backend): escape the projects in the jql string (#276)
    This changes escapes each project key project key in the jql string.
    This is to handle projects that contains reserved jql strings, for
    example "DO".

---
+++ .changeset/old-coins-lick.md
@@ -0,0 +1,5 @@
+---
+'@axis-backstage/plugin-jira-dashboard-backend': patch
+---
+
+Each project key in the JQL query are now wrapped in single qoutes to handle projects that contains reserved JQL words.
--- plugins/jira-dashboard-backend/src/queries.test.ts
+++ plugins/jira-dashboard-backend/src/queries.test.ts
@@ -1,20 +1,53 @@
-import { jqlQueryBuilder } from './queries';
+import { createEscapedIncludeQuery, jqlQueryBuilder } from './queries';
 
 describe('queries', () => {
-  it('can use all arguments build a query.', async () => {
-    const jql = jqlQueryBuilder({
-      project: 'BS,SB',
-      components: ['comp 1', 'comp 2'],
-      query: 'filter=example',
+  describe('jqlQueryBuilder', () => {
+    it('can use all arguments build a query.', async () => {
+      const jql = jqlQueryBuilder({
+        project: ['BS', 'SB'],
+        components: ['comp 1', 'comp 2'],
+        query: 'filter=example',
+      });
+      expect(jql).toBe(
+        "project in ('BS','SB') AND component in ('comp 1','comp 2') AND filter=example",
+      );
     });
-    expect(jql).toBe(
-      "project in (BS,SB) AND component in ('comp 1','comp 2') AND filter=example",
-    );
+
+    it('can create a query using only a project as argument.', async () => {
+      const jql = jqlQueryBuilder({
+        project: ['BS'],
+      });
+      expect(jql).toBe("project in ('BS')");
+    });
+
+    it('can create a query with multiple projects and no components or additional query.', async () => {
+      const jql = jqlQueryBuilder({
+        project: ['BS', 'SB'],
+      });
+      expect(jql).toBe("project in ('BS','SB')");
+    });
+
+    it('can create a query with an additional query but no components.', async () => {
+      const jql = jqlQueryBuilder({
+        project: ['BS'],
+        query: 'filter=example',
+      });
+      expect(jql).toBe("project in ('BS') AND filter=example");
+    });
   });
-  it('can create a query using only a project as argument.', async () => {
-    const jql = jqlQueryBuilder({
-      project: 'BS',
+
+  describe('createEscapedIncludeQuery', () => {
+    it('creates a query with a single value', () => {
+      const query = createEscapedIncludeQuery('affectedVersion', ['3.14']);
+      expect(query).toBe("affectedVersion in ('3.14')");
     });
-    expect(jql).toBe('project in (BS)');
+
+    it('creates a query with multiple values', () => {
+      const query = createEscapedIncludeQuery('affectedVersion', [
+        '3.14',
+        '4.2',
+      ]);
+      expect(query).toBe("affectedVersion in ('3.14','4.2')");
+    });
   });
 });
