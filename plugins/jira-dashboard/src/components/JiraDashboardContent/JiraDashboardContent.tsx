820fb8c42c61d4d904a4c09a9e4b1e5dd98dc1b8
enaysaa <saachi.nayyer@ericsson.com>
2025-07-31 15:41:14 +0100

    Improve Responsiveness of Jira Dashboard #302 Signed-off-by: enaysaa saachi.nayyer@ericsson.com

--- plugins/jira-dashboard/src/components/JiraDashboardContent/JiraDashboardContent.tsx
+++ plugins/jira-dashboard/src/components/JiraDashboardContent/JiraDashboardContent.tsx
@@ -9,21 +9,15 @@ import {
   TabbedCard,
   CardTab,
 } from '@backstage/core-components';
-import Grid from '@mui/material/Unstable_Grid2';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import { JiraProjectCard } from '../JiraProjectCard';
-import { JiraTable } from '../JiraTable';
 import { useApi } from '@backstage/core-plugin-api';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { stringifyEntityRef } from '@backstage/catalog-model';
 import { jiraDashboardApiRef } from '../../api';
 import { useJira } from '../../hooks/useJira';
-import {
-  Issue,
-  JiraDataResponse,
-} from '@axis-backstage/plugin-jira-dashboard-common';
+import { Issue } from '@axis-backstage/plugin-jira-dashboard-common';
+import { JiraGrid } from './JiraGrid';
 
 export const JiraDashboardContent = (props?: {
   showFilters?: TableFilter[] | boolean;
@@ -38,11 +32,9 @@ export const JiraDashboardContent = (props?: {
     error,
   } = useJira(stringifyEntityRef(entity), api);
 
-  if (loading) {
-    return <Progress />;
-  } else if (error) {
-    return <ResponseErrorPanel error={error} />;
-  } else if (!jiraResponse || !jiraResponse?.data || !jiraResponse?.project) {
+  if (loading) return <Progress />;
+  if (error) return <ResponseErrorPanel error={error} />;
+  if (!jiraResponse?.data || !jiraResponse?.project) {
     return (
       <ResponseErrorPanel
         error={Error(
@@ -51,6 +43,7 @@ export const JiraDashboardContent = (props?: {
       />
     );
   }
+
   const projects = Array.isArray(jiraResponse.project)
     ? jiraResponse.project
     : [];
@@ -59,78 +52,39 @@ export const JiraDashboardContent = (props?: {
     <Content>
       <ContentHeader title="Jira Dashboard">
         <SupportButton>
-          <Box>
-            <Typography variant="h6">How it works</Typography>
-            The Jira Dashboard plugin provides quickly access to Jira issue
-            summaries. You can add Jira components and filters of your choice by
-            defining annotations to your entity's catalog-info.yaml file.
-            <Typography mt={1} variant="h6">
-              Documentation
-            </Typography>
-            You find the documentation{' '}
-            <Link href="https://github.com/AxisCommunications/backstage-plugins/tree/main/plugins/jira-dashboard">
-              here.
-            </Link>
-          </Box>
+          <Typography variant="h6">How it works</Typography>
+          The Jira Dashboard plugin provides quick access to Jira issue
+          summaries. Add Jira components and filters via annotations in your
+          entity's `catalog-info.yaml`.
+          <Typography mt={1} variant="h6">
+            Documentation
+          </Typography>
+          <Link href="https://github.com/AxisCommunications/backstage-plugins/tree/main/plugins/jira-dashboard">
+            here.
+          </Link>
         </SupportButton>
       </ContentHeader>
+
       {projects.length > 1 ? (
         <TabbedCard title="Jira Projects" data-testid="tabbed-card">
           {projects.map(project => (
             <CardTab key={project.key} label={project.name}>
-              <Grid container spacing={3}>
-                <Grid md={6} xs={12} data-testid="project-card">
-                  <JiraProjectCard project={project} />
-                </Grid>
-                {jiraResponse.data.map(value => (
-                  <Grid
-                    data-testid="issue-table"
-                    key={value.name}
-                    md={6}
-                    xs={12}
-                  >
-                    <JiraTable
-                      tableContent={value}
-                      showFilters={props?.showFilters}
-                      project={project}
-                      tableOptions={props?.tableOptions}
-                      tableStyle={{
-                        height: 'max-content',
-                        maxHeight: '500px',
-                        padding: '20px',
-                        overflowY: 'auto',
-                        width: '100%',
-                      }}
-                    />
-                  </Grid>
-                ))}
-              </Grid>
+              <JiraGrid
+                project={project}
+                tableData={jiraResponse.data}
+                showFilters={props?.showFilters}
+                tableOptions={props?.tableOptions}
+              />
             </CardTab>
           ))}
         </TabbedCard>
       ) : (
-        <Grid container spacing={3} data-testid="single-project-view">
-          <Grid md={6} xs={12} data-testid="project-card">
-            <JiraProjectCard project={projects[0]} />
-          </Grid>
-          {jiraResponse.data.map((value: JiraDataResponse) => (
-            <Grid data-testid="issue-table" key={value.name} md={6} xs={12}>
-              <JiraTable
-                tableContent={value}
-                showFilters={props?.showFilters}
-                project={projects[0]}
-                tableOptions={props?.tableOptions}
-                tableStyle={{
-                  height: 'max-content',
-                  maxHeight: '500px',
-                  padding: '20px',
-                  overflowY: 'auto',
-                  width: '100%',
-                }}
-              />
-            </Grid>
-          ))}
-        </Grid>
+        <JiraGrid
+          project={projects[0]}
+          tableData={jiraResponse.data}
+          showFilters={props?.showFilters}
+          tableOptions={props?.tableOptions}
+        />
       )}
     </Content>
   );
