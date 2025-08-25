a04b26a14061f1c517571d8fc9929e68c4bd16ad
Saachi Nayyer <106317658+SaachiNayyer@users.noreply.github.com>
2025-08-25 11:55:04 +0100

    Introduce profile card on assignee hover in Jira Dashboard #300 (#301)
    Signed-off-by: enaysaa saachi.nayyer@ericsson.com
    
    Signed-off-by: enaysaa saachi.nayyer@ericsson.com

---
+++ plugins/jira-dashboard/src/components/JiraTable/cells/AssigneeCell.tsx
@@ -0,0 +1,66 @@
+import { Avatar, Link } from '@backstage/core-components';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import { Issue } from '@axis-backstage/plugin-jira-dashboard-common';
+import { EntityPeekAheadPopover } from '@backstage/plugin-catalog-react';
+import { stringifyEntityRef } from '@backstage/catalog-model';
+
+type Props = {
+  assignee?: Issue['fields']['assignee'];
+};
+
+const normalizeAssigneeName = (name: string): string => {
+  if (!name) return '';
+  if (name.includes('@')) {
+    return name.split('@')[0];
+  }
+  return name;
+};
+export const AssigneeCell = ({ assignee }: Props) => {
+  if (!assignee) {
+    return null;
+  }
+
+  if (!assignee.name || assignee.name.toLowerCase() === 'unassigned') {
+    return (
+      <Stack
+        direction="row"
+        gap={1}
+        alignItems="center"
+        data-testid="assignee-avatar"
+      >
+        <Avatar picture="" customStyles={{ width: 35, height: 35 }} />
+        <Typography variant="body2">Unassigned</Typography>
+      </Stack>
+    );
+  }
+
+  const entityRef = {
+    kind: 'user',
+    namespace: 'default',
+    name: normalizeAssigneeName(assignee.name),
+  };
+
+  return (
+    <EntityPeekAheadPopover entityRef={stringifyEntityRef(entityRef)}>
+      <Link
+        to={`/catalog/${entityRef.namespace}/${entityRef.kind}/${entityRef.name}`}
+      >
+        <Stack
+          direction="row"
+          gap={1}
+          alignItems="center"
+          data-testid="assignee-avatar"
+        >
+          <Avatar
+            picture={assignee.avatarUrls?.['48x48'] || ''}
+            customStyles={{ width: 35, height: 35 }}
+          />
+          <Typography noWrap variant="body2">
+            {assignee.displayName || assignee.name}
+          </Typography>
+        </Stack>
+      </Link>
+    </EntityPeekAheadPopover>
+  );
+};
