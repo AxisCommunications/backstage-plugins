ce7eaa6b06f30849bef4ec0a2a903266990b76e6
Vincenzo Scamporlino <vincenzos@spotify.com>
2025-09-04 13:37:51 +0200

    chore(app-next): remove default api definition
    Signed-off-by: Vincenzo Scamporlino <vincenzos@spotify.com>

--- packages/app-next/src/App.tsx
+++ packages/app-next/src/App.tsx
@@ -1,19 +1,13 @@
 import { createApp } from '@backstage/frontend-defaults';
 import {
-  configApiRef,
-  ApiBlueprint,
   createFrontendModule,
   PageBlueprint,
 } from '@backstage/frontend-plugin-api';
-import {
-  ScmIntegrationsApi,
-  scmIntegrationsApiRef,
-} from '@backstage/integration-react';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import catalogImportPlugin from '@backstage/plugin-catalog-import/alpha';
 import userSettingsPlugin from '@backstage/plugin-user-settings/alpha';
 import { Navigate } from 'react-router';
-// if app.experimental is not set in the app config, could manually add features like this
+// if app.packages is not set in the app config, could manually add features like this
 // See documentation for details: https://backstage.io/docs/frontend-system/architecture/app#feature-discovery
 // import jiraPlugin from '@axis-backstage/plugin-jira-dashboard/alpha';
 
@@ -25,25 +19,14 @@ const homePageExtension = PageBlueprint.make({
   },
 });
 
-const scmIntegrationsApi = ApiBlueprint.make({
-  name: 'scm-integrations',
-  params: defineParams =>
-    defineParams({
-      api: scmIntegrationsApiRef,
-      deps: { configApi: configApiRef },
-      factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
-    }),
-});
-
 export const app = createApp({
   features: [
     catalogPlugin,
     catalogImportPlugin,
     userSettingsPlugin,
-    // jiraPlugin,
     createFrontendModule({
       pluginId: 'app',
-      extensions: [homePageExtension, scmIntegrationsApi],
+      extensions: [homePageExtension],
     }),
   ],
 });
