6565164a211eecd36a076c1d23582de638170a80
Frida Englund <fridaja@axis.com>
2026-04-20 14:41:50 +0200

    feat(jira-dashboard): remove ContentHeader and Content wrapper

--- plugins/jira-dashboard/src/components/JiraDashboardContent/JiraDashboardContent.tsx
+++ plugins/jira-dashboard/src/components/JiraDashboardContent/JiraDashboardContent.tsx
@@ -1,16 +1,11 @@
 import {
-  Content,
-  ContentHeader,
   Progress,
   ResponseErrorPanel,
-  SupportButton,
   TableFilter,
   TableOptions,
   TabbedCard,
   CardTab,
 } from '@backstage/core-components';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 import { useApi } from '@backstage/core-plugin-api';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { stringifyEntityRef } from '@backstage/catalog-model';
@@ -49,22 +44,7 @@ export const JiraDashboardContent = (props?: {
     : [];
 
   return (
-    <Content>
-      <ContentHeader title="Jira Dashboard">
-        <SupportButton>
-          <Typography variant="h6">How it works</Typography>
-          The Jira Dashboard plugin provides quick access to Jira issue
-          summaries. Add Jira components and filters via annotations in your
-          entity's `catalog-info.yaml`.
-          <Typography mt={1} variant="h6">
-            Documentation
-          </Typography>
-          <Link href="https://github.com/AxisCommunications/backstage-plugins/tree/main/plugins/jira-dashboard">
-            here.
-          </Link>
-        </SupportButton>
-      </ContentHeader>
-
+    <>
       {projects.length > 1 ? (
         <TabbedCard title="Jira Projects" data-testid="tabbed-card">
           {projects.map(project => (
@@ -86,6 +66,6 @@ export const JiraDashboardContent = (props?: {
           tableOptions={props?.tableOptions}
         />
       )}
-    </Content>
+    </>
   );
 };
